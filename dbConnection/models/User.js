c27cf845cdc3bfe58def8e31928eb8fa3b0adc95
Diane Wu <dianewudw@gmail.com>
2019-10-12 10:35:20 -0400

    comments in db and server

--- dbConnection/models/User.js
+++ dbConnection/models/User.js
@@ -1,14 +1,23 @@
+//loads module onto page
 const Sequelize = require('sequelize');
+//loads page connectToDb onto the page
 const sequelize = require('../connectToDb');
 
+//creates a model/framework for user
 const User = sequelize.define('user', {
+  //creates a specific ID for user
   _id: {
+    //generates UUID value
     type: Sequelize.UUID,
+    //value is an actual UUID value
     defaultValue: Sequelize.UUIDV1,
     primaryKey: true
   },
+  //username from github
   username: Sequelize.STRING,
+  //avatar URL from github
   avatar: Sequelize.STRING,
+  //ranking/seeding in tournament
   points: Sequelize.INTEGER,
   created_at: {
     type: 'TIMESTAMP',
@@ -23,5 +32,5 @@ const User = sequelize.define('user', {
 });
 
 // sequelize.sync({ logging: console.log }).then(() => {});
-
+//allows exporting of model --> User
 module.exports = User;
