b637698c404a0ef6ce726894b8f5c91eb65b62de
alexchao26 <alexchaoo@gmail.com>
2019-10-12 14:05:30 -0400

    models created

--- dbConnection/connectToDb.js
+++ dbConnection/connectToDb.js
@@ -1,31 +1,31 @@
 // postgres://chybszjv:PvlzLd7rzW50D56qb4tjHpJ5rZ9tt8kd@salt.db.elephantsql.com:5432/chybszjv
-//imports moduel onto page
+// imports moduel onto page
 const Sequelize = require('sequelize');
-//opens connection to ElephantSQL url
+// opens connection to ElephantSQL url
 const sequelize = new Sequelize(
-  //username
+  // username
   'chybszjv',
-  //passworkd
+  // passworkd
   'chybszjv',
-  //url
+  // url
   'PvlzLd7rzW50D56qb4tjHpJ5rZ9tt8kd',
   {
-    //hostname
+    // hostname
     host: 'salt.db.elephantsql.com',
-    //speaking in postgres
-    dialect: 'postgres'
-  }
+    // speaking in postgres
+    dialect: 'postgres',
+  },
 );
 
-//authenticates acccess to database
+// authenticates acccess to database
 sequelize
   .authenticate()
-  //promise: after authentication, let it be known that the connection was established
+  // promise: after authentication, let it be known that the connection was established
   .then(() => {
     console.log('Connection has been established successfully.');
   })
-  //else: show what the error in connect is
-  .catch(err => {
+  // else: show what the error in connect is
+  .catch((err) => {
     console.error('Unable to connect to the database:', err);
   });
 
--- dbConnection/models/User.js
+++ dbConnection/models/User.js
@@ -1,36 +1,36 @@
-//loads module onto page
+// loads module onto page
 const Sequelize = require('sequelize');
-//loads page connectToDb onto the page
+// loads page connectToDb onto the page
 const sequelize = require('../connectToDb');
 
-//creates a model/framework for user
+// creates a model/framework for user
 const User = sequelize.define('user', {
-  //creates a specific ID for user
+  // creates a specific ID for user
   _id: {
-    //generates UUID value
+    // generates UUID value
     type: Sequelize.UUID,
-    //value is an actual UUID value
+    // value is an actual UUID value
     defaultValue: Sequelize.UUIDV1,
-    primaryKey: true
+    primaryKey: true,
   },
-  //username from github
+  // username from github
   username: Sequelize.STRING,
-  //avatar URL from github
+  // avatar URL from github
   avatar: Sequelize.STRING,
-  //ranking/seeding in tournament
+  // ranking/seeding in tournament
   points: Sequelize.INTEGER,
   created_at: {
     type: 'TIMESTAMP',
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    allowNull: false
+    allowNull: false,
   },
   updated_at: {
     type: 'TIMESTAMP',
     defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-    allowNull: false
-  }
+    allowNull: false,
+  },
 });
 
 // sequelize.sync({ logging: console.log }).then(() => {});
-//allows exporting of model --> User
+// allows exporting of model --> User
 module.exports = User;
