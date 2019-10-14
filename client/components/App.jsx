d3d356756f4e85a4bdf82a60b56312c0f2f73ed5
github username <github email address>
2019-10-14 15:29:17 -0400

    trying to resolve conflicts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,19 @@
 /* eslint-disable import/extensions */
 import React from 'react';
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { Route, Link } from 'react-router-dom';
 
-import Splash from './Splash';
-import MainContainer from './MainContainer';
-import Tournament from './Tournament';
-
+import MainContainer from './MainContainer.jsx'
+import Splash from './Splash.jsx'
 import '../styles.css';
-import { logIn } from '../actions/actionCreator';
+import { logIn } from "../actions/actionCreator";
 
 const mapStateToProps = function(state) {
-  console.log('STATE', state);
+  console.log("STATE", state)
   return {
     isLoggedIn: state.isLoggedIn
-  };
-};
+  }
+}
 
 function mapDispatchToProps(dispatch) {
   return {
@@ -23,34 +21,32 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-class App extends React.Component {
+class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      isLoggedIn: this.props.isLoggedIn
+      isLoggedIn: this.props.isLoggedIn,
     };
     this.handleClick = this.handleClick.bind(this);
   }
-
-  handleClick(e) {
+  
+  handleClick(e){
     // this.setState({isLoggedIn:true}, console.log(this.state))
     // this.props.logIn()
     // this.setState({isLoggedIn:this.props.isLoggedIn})
     // setInterval(console.log(this.props.isLoggedIn), 2000)
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Route exact path='/' component={Splash} />
-        <Route path='/main' component={MainContainer} />
-        <Route path='/tournaments' component={Tournament} />
-      </React.Fragment>
-    );
+  render(){
+      return (
+        <React.Fragment>
+          <Route exact path="/" component={Splash} />
+          <Route path="/main" component={MainContainer} />
+          <Route path="/tournaments" component={Tournament} />
+        </React.Fragment>
+      );
+    
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
