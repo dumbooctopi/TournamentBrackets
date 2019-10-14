fbe6d2e33ca69d16ee177e7f202225cf7690d9bf
alexchao26 <alexchaoo@gmail.com>
2019-10-13 21:34:19 -0400

    endpoints set up for getting all matches from a tournament, and creating a tournament (which adds four matches for round one and adds a row to the tournaments table).

--- dbConnection/models/User.js
+++ dbConnection/models/User.js
@@ -2,19 +2,20 @@ const Sequelize = require('sequelize');
 const sequelize = require('../connectToDb');
 
 const User = sequelize.define('user', {
-  _id: {
-    type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV1,
-    primaryKey: true
-  },
+  // commented out to get this to just be the default sql primary id
+  // _id: {
+  //   type: Sequelize.UUID,
+  //   defaultValue: Sequelize.UUIDV1,
+  //   primaryKey: true
+  // },
   username: Sequelize.STRING,
   avatar: Sequelize.STRING,
   points: Sequelize.INTEGER,
   role: {
     type: Sequelize.STRING,
     defaultValue: 'user',
-    allowNull: false
-  }
+    allowNull: false,
+  },
 });
 
 module.exports = User;
